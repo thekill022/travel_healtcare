9e3364f26531b5e73738f46a7e2904ece904ff08
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-11-20 20:48:56 +0700

    menampilkan symptoms dengan list

--- lib/model/SymptomModel.dart
+++ lib/model/SymptomModel.dart
@@ -1,41 +1,40 @@
 import 'dart:convert';
 
 class SymptomModel {
+  var id;
   final String symptomName;
   final String symptomChar;
-  var id;
-
   SymptomModel({
+    this.id,
     required this.symptomName,
     required this.symptomChar,
-    required this.id,
   });
 
   SymptomModel copyWith({
+    int? id,
     String? symptomName,
     String? symptomChar,
-    int? id,
   }) {
     return SymptomModel(
+      id: id ?? this.id,
       symptomName: symptomName ?? this.symptomName,
       symptomChar: symptomChar ?? this.symptomChar,
-      id: id ?? this.id,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id.toMap(),
       'symptom_name': symptomName,
       'symptom_char': symptomChar,
-      'id': id.toMap(),
     };
   }
 
   factory SymptomModel.fromMap(Map<String, dynamic> map) {
     return SymptomModel(
+      id: map['id'] ?? '',
       symptomName: map['symptom_name'] ?? '',
       symptomChar: map['symptom_char'] ?? '',
-      id: map['id'] ?? '',
     );
   }
 
@@ -46,18 +45,18 @@ class SymptomModel {
 
   @override
   String toString() =>
-      'SymptomModel(symptomName: $symptomName, symptomChar: $symptomChar, id: $id)';
+      'SymptomModel(id: $id, symptomName: $symptomName, symptomChar: $symptomChar)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
     return other is SymptomModel &&
+        other.id == id &&
         other.symptomName == symptomName &&
-        other.symptomChar == symptomChar &&
-        other.id == id;
+        other.symptomChar == symptomChar;
   }
 
   @override
-  int get hashCode => symptomName.hashCode ^ symptomChar.hashCode ^ id.hashCode;
+  int get hashCode => id.hashCode ^ symptomName.hashCode ^ symptomChar.hashCode;
 }
--- lib/views/post_travel.dart
+++ lib/views/post_travel.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:travel_healthcare/controller/symptom_controller.dart';
 import 'package:travel_healthcare/model/SymptomModel.dart';
+import 'package:http/http.dart' as http;
 
 class PostTravelPage extends StatefulWidget {
-  const PostTravelPage({super.key});
+  const PostTravelPage({Key? key}) : super(key: key);
 
   @override
   State<PostTravelPage> createState() => _PostTravelPageState();
@@ -14,11 +16,40 @@ class _PostTravelPageState extends State<PostTravelPage> {
   List<SymptomModel> listsymptom = [];
 
   String? selectSymptom;
+  final String apiUrl = 'http://10.0.2.2:5000/api/symptoms/';
+  String? stringResponse;
+
+  Future symptomCall() async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String? token = prefs.getString('token');
+
+      if (token == null) {
+        throw Exception('Bearer token not found in SharedPreferences');
+      }
+
+      final response = await http.get(
+        Uri.parse(apiUrl),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          stringResponse = response.body;
+        });
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     // Call the function to fetch symptoms when the widget is created
+    symptomCall();
     fetchSymptoms();
   }
 
@@ -35,55 +66,55 @@ class _PostTravelPageState extends State<PostTravelPage> {
     }
   }
 
-  List<DropdownMenuItem> generateItems(List<String> list) {
-    List<DropdownMenuItem> items = [];
-    for (var item in list) {
-      items.add(DropdownMenuItem(
-        child: Text(item),
-        value: item,
-      ));
-    }
-    return items;
+  List<DropdownMenuItem<SymptomModel>> generateItems(List<SymptomModel> list) {
+    return list.map((symptom) {
+      return DropdownMenuItem(
+        child: Text(symptom.symptomChar),
+        value: symptom,
+      );
+    }).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView.builder(
-        itemCount: listsymptom.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Container(
-              child: Column(
-                children: [
-                  Text(listsymptom[index].symptomName),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.only(left: 10),
-                    margin: const EdgeInsets.only(right: 250),
-                    decoration: BoxDecoration(
+        child: ListView.builder(
+          itemCount: listsymptom.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Container(
+                child: Column(
+                  children: [
+                    Text(listsymptom[index].symptomName),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.only(left: 10),
+                      margin: const EdgeInsets.only(right: 250),
+                      decoration: BoxDecoration(
                         border: Border.all(color: Colors.black, width: 1),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: DropdownButton(
-                      borderRadius: BorderRadius.circular(10),
-                      icon: const Icon(Icons.arrow_drop_down_circle_rounded),
-                      dropdownColor: const Color.fromARGB(255, 223, 121, 238),
-                      value: selectSymptom,
-                      items: generateItems(listsymptom.cast<String>()),
-                      onChanged: (item) {
-                        setState(() {
-                          selectSymptom = item;
-                        });
-                      },
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: DropdownButton(
+                        borderRadius: BorderRadius.circular(10),
+                        icon: const Icon(Icons.arrow_drop_down_circle_rounded),
+                        dropdownColor: const Color.fromARGB(255, 223, 121, 238),
+                        value: selectSymptom,
+                        items: generateItems(listsymptom),
+                        onChanged: (item) {
+                          setState(() {
+                            selectSymptom = item as String?;
+                          });
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-      )),
+            );
+          },
+        ),
+      ),
     );
   }
 }
