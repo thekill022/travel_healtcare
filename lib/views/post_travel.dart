0a570257a324d8c89c230cd0b750f16060c9b314
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-11-23 14:58:27 +0700

    membuat tampilan gejala dan berhasil menampilkan dropdown

--- lib/views/post_travel.dart
+++ lib/views/post_travel.dart
@@ -1,10 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:travel_healthcare/controller/symptom_controller.dart';
 import 'package:travel_healthcare/model/SymptomModel.dart';
-import 'package:http/http.dart' as http;
 
 class PostTravelPage extends StatefulWidget {
   const PostTravelPage({Key? key}) : super(key: key);
@@ -17,41 +13,80 @@ class _PostTravelPageState extends State<PostTravelPage> {
   SymptomController symptomController = SymptomController();
   //List<SymptomModel> listsymptom = [];
 
+  late Future<List<SymptomModel>> _symptoms;
   @override
   void initState() {
     super.initState();
-    symptomController.getSymptoms();
+    _symptoms = symptomController.getSymptoms();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Symptom List'),
-      ),
-      body: FutureBuilder<List<SymptomModel>>(
-        future: symptomController.getSymptoms(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else {
-            List<SymptomModel> symptoms = snapshot.data!;
-            return ListView.builder(
-              itemCount: symptoms.length,
-              itemBuilder: (context, index) {
-                SymptomModel symptom = symptoms[index];
-                return ListTile(
-                  title: Text(symptom.symptomName),
-                  subtitle: Text(symptom.symptomChar),
-                );
-              },
-            );
-          }
-        },
+      body: SafeArea(
+        child: FutureBuilder<List<SymptomModel>>(
+          future: _symptoms,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.lightBlueAccent,
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else {
+              List<SymptomModel> symptoms = snapshot.data!;
+
+              // Use toSet() to remove duplicates based on symptomName
+              List<SymptomModel> distinctSymptoms = symptoms.toSet().toList();
+
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  itemCount: distinctSymptoms.length,
+                  itemBuilder: (context, index) {
+                    SymptomModel symptom = distinctSymptoms[index];
+                    return Card(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              symptom.symptomName,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          DropdownButtonFormField<SymptomModel>(
+                            hint: const Text('Select a symptom'),
+                            value: null, // Initially, no value is selected
+                            onChanged: (SymptomModel? selectedSymptom) {
+                              // Handle selected value
+                            },
+                            items: symptoms
+                                .where(
+                                    (s) => s.symptomName == symptom.symptomName)
+                                .map((SymptomModel s) {
+                              return DropdownMenuItem<SymptomModel>(
+                                value: s,
+                                child: Text(s.symptomChar),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
